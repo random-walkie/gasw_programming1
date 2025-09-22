e3f7679d74b5411911c135ec2a7594dcc506e6a3
random-walkie <26143642+random-walkie@users.noreply.github.com>
2025-09-22 13:30:55 +0100

    Update MainShapes to add new circle, print object details, and demonstrate visibility

--- week2/lab/shapes/src/MainShapes.java
+++ week2/lab/shapes/src/MainShapes.java
@@ -26,6 +26,14 @@ public class MainShapes {
         // Task 1.12: Change the color of circle1 to green
         circle1.changeColor("green");
 
+        // Task 1.13: Create a new circle and make it visible.
+        Circle circle2 = new Circle();
+        circle2.makeVisible();
+
+        // Task 1.14: Print the details of objects
+        System.out.println(circle1);
+        System.out.println(circle2);
+        System.out.println(square1);
 
     }
 }
