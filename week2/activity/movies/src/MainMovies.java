c932b25879b29200ad72806b59c25a3f6d2bbbdd
random-walkie <26143642+random-walkie@users.noreply.github.com>
2025-09-22 12:04:36 +0100

    Update MainMovies to demonstrate Movie class usage

--- week2/activity/movies/src/MainMovies.java
+++ week2/activity/movies/src/MainMovies.java
@@ -1,5 +1,24 @@
 public class MainMovies {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Movie movie1 = new Movie();
+        // Print movie1 properties
+        System.out.println(movie1);
+
+        Movie movie2 = new Movie("The Godfather", 175);
+        // Print movie2 properties
+        System.out.println(movie2);
+
+        // Print movie2 running time
+        System.out.println(movie2.getRunningTime());
+
+        // Play movie2
+        movie2.play();
+        System.out.println(movie2);
+        // Stop movie2
+        movie2.stop();
+        System.out.println(movie2);
+        // Resume movie2
+        movie2.resume();
+        System.out.println(movie2);
     }
 }
