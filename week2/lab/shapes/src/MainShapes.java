75e1f1a3297544858c47db797ce5d1517b0b08d9
random-walkie <26143642+random-walkie@users.noreply.github.com>
2025-09-22 13:19:41 +0100

    Update MainShapes to demonstrate shape manipulation and movement

--- week2/lab/shapes/src/MainShapes.java
+++ week2/lab/shapes/src/MainShapes.java
@@ -1,5 +1,31 @@
 public class MainShapes {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Create a circle and make it visible.
+        Circle circle1 = new Circle();
+        circle1.makeVisible();
+
+        // Create a square and make it visible.
+        Square square1 = new Square();
+        square1.makeVisible();
+
+        // Task 1.8: Call moveDown method of circle1
+        circle1.moveDown();
+
+        // Task 1.9: Call moveRight method of circle1 twice
+        circle1.moveRight();
+        circle1.moveRight();
+
+        // Task 1.10: Call moveHorizontal method of square1
+        square1.moveHorizontal(100);
+
+        // Task 1.11: Move the square 10 pixels down and 50 pixels to the left,
+        // with one method call for each
+        square1.moveVertical(-10);
+        square1.moveHorizontal(-50);
+
+        // Task 1.12: Change the color of circle1 to green
+        circle1.changeColor("green");
+
+
     }
 }
